06b3531bab937c703bdd670587be0d2062c4c922
Taylor Caton <taylorcaton@gmail.com>
2017-07-14 22:54:03 -0400

    Twitter auth

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -133,27 +133,12 @@ function initChat(user) {
 }
 
 function login() {
-
-  var provider = new firebase.auth.GoogleAuthProvider();
-  //Sign-in with Google
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // ...
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-    debugger;
-  });
-}
+    // Log the user in via Twitter
+    var provider = new firebase.auth.TwitterAuthProvider();
+    firebase.auth().signInWithPopup(provider).catch(function(error) {
+      console.log("Error authenticating user:", error);
+    });
+  }
 
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
@@ -166,12 +151,6 @@ firebase.auth().onAuthStateChanged(function(user) {
     // ...
   }
   // ...
-});
-
-firebase.auth().signOut().then(function() {
-  // Sign-out successful.
-}).catch(function(error) {
-  // An error happened.
 });
 
 $(document).on("click","#auth",function(){
